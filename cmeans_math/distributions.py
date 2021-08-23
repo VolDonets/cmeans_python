7a182dd6de4e6483d7ac50f0148fe21374d2566c
VolDonets <vovan.s.marsa@gmail.com>
2021-08-23 19:36:56 +0300

    I have added an old distribution function for using in membership function.

--- cmeans_math/distributions.py
+++ cmeans_math/distributions.py
@@ -4,3 +4,12 @@ import math
 def cauchy_distribution(var_etta, var_distance):
     var_tmp = math.pi * var_etta * (1 + (var_distance / (var_etta * var_etta)))
     return 1 / var_tmp
+
+
+def own_old_distribution(var_etta, var_betta, var_distance):
+    var_pow = 1 / (1 - var_betta)
+    var_tmp = var_distance / (var_etta * var_etta)
+    var_tmp = math.pow(var_tmp, var_pow)
+    var_tmp = var_tmp + 1
+    var_tmp = 1 / var_tmp
+    return var_tmp
