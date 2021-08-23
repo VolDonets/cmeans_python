98c42f0a6fd7ea7e0aacbfe002de2301280e3255
VolDonets <vovan.s.marsa@gmail.com>
2021-08-23 19:41:15 +0300

    I have used numpy.cov() function for calculating covariance matrix instead doing it manually as it was in C++ project. BUT here is totally different math, I didn't test it yet, and I can't say any thing about it behaviour.

---
+++ cmeans_math/covariances.py
@@ -0,0 +1,29 @@
+import numpy as np
+from scipy import linalg
+
+
+ARRAY_LAMBDA = 0.001
+
+
+def np_covariance_matrix(mat_entries, vec_cluster_indexes):
+    mat_cluster_entries = []
+    for inx in vec_cluster_indexes:
+        mat_cluster_entries.append(mat_entries[inx])
+    npmat_cluster_entries = np.array(mat_cluster_entries).T
+    npmat_covariance = np.cov(npmat_cluster_entries)
+    return npmat_covariance
+
+
+def np_inverse_covariance(npmat_covariance):
+    for i in range(len(npmat_covariance)):
+        npmat_covariance[i][i] += ARRAY_LAMBDA
+    return linalg.inv(npmat_covariance)
+
+
+def cluster_covariances(mat_entries, ten_cluster_indexes):
+    ten_covariances = []
+    for i in range(len(ten_cluster_indexes)):
+        npmat_covariance = np_covariance_matrix(mat_entries, ten_cluster_indexes[i])
+        npmat_covariance = np_inverse_covariance(npmat_covariance)
+        ten_covariances.append(npmat_covariance.tolist())
+    return ten_covariances
