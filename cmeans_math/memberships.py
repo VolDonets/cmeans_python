a1d3a0f5f4220f5c6407bec009b4e761fefeb526
VolDonets <vovan.s.marsa@gmail.com>
2021-09-05 17:38:05 +0300

    I have added documentation for memberships.

--- cmeans_math/memberships.py
+++ cmeans_math/memberships.py
@@ -1,8 +1,32 @@
+"""Memberships
+
+This script contains functions for generating membership matrix via different methods.
+
+Functions:
+    * euclid_membership_matrix - calculates a membership matrix based on euclid distance
+    * manhattan_membership_matrix - calculates a membership matrix based on manhattan distance
+    * mahalanobis_membership_matrix - calculates a membership matrix based on mahalanobis distance
+    * mahalanobis_cauchy_membership_matrix - calculates a membership matrix based on cauchy distribution and
+                                             mahalanobis distance
+    * mahalanobis_own_old_distribution - calculates a membership matrix based on own distribution and
+                                         mahalanobis distance
+    * norm_membership_matrix_for_clusters - recalculate membership matrix to normalize it's values
+    * norm_membership_matrix_for_entries - recalculate membership matrix to normalize it's values
+"""
+
 import distances
 import distributions
 
 
 def euclid_membership_matrix(mat_entries, mat_cluster_centers):
+    """
+    Here each membership_value = euclid_distance(entry_vector, cluster_center_vector)
+
+    :param mat_entries: vector of entry vectors (matrix), len(entry_vector) = len(cluster_center_vector)
+    :param mat_cluster_centers: vector of cluster centers vectors (matrix)
+    :return: membership matrix (vector of cluster vector membership values)
+    """
+
     mat_membership = []
     for vec_cluster_center in mat_cluster_centers:
         vec_cluster_distances = []
@@ -14,6 +38,14 @@ def euclid_membership_matrix(mat_entries, mat_cluster_centers):
 
 
 def manhattan_membership_matrix(mat_entries, mat_cluster_centers):
+    """
+    Here each membership_value = manhattan_distance(entry_vector, cluster_center_vector)
+
+    :param mat_entries: vector of entry vectors (matrix), len(entry_vector) = len(cluster_center_vector)
+    :param mat_cluster_centers: vector of cluster centers vectors (matrix)
+    :return: membership matrix (vector of cluster vector membership values)
+    """
+
     mat_membership = []
     for vec_cluster_center in mat_cluster_centers:
         vec_cluster_distances = []
@@ -25,6 +57,15 @@ def manhattan_membership_matrix(mat_entries, mat_cluster_centers):
 
 
 def mahalanobis_membership_matrix(mat_entries, mat_cluster_centers, ten_covariances):
+    """
+    Here each membership_value = manhattan_distance(entry_vector, cluster_center_vector)
+
+    :param mat_entries: vector of entry vectors (matrix), len(entry_vector) = len(cluster_center_vector)
+    :param mat_cluster_centers: vector of cluster centers vectors (matrix)
+    :param ten_covariances: vector of covariance matrices
+    :return: membership matrix (vector of cluster vector membership values)
+    """
+
     mat_membership = []
     for i in range(len(mat_cluster_centers)):
         vec_cluster_distances = []
@@ -37,6 +78,15 @@ def mahalanobis_membership_matrix(mat_entries, mat_cluster_centers, ten_covarian
 
 
 def mahalanobis_cauchy_membership_matrix(mat_entries, mat_cluster_centers, ten_covariances):
+    """
+    Here each membership_value = cauchy_distribution(1, manhattan_distance(entry_vector, cluster_center_vector))
+
+    :param mat_entries: vector of entry vectors (matrix), len(entry_vector) = len(cluster_center_vector)
+    :param mat_cluster_centers: vector of cluster centers vectors (matrix)
+    :param ten_covariances: vector of covariance matrices
+    :return: membership matrix (vector of cluster vector membership values)
+    """
+
     mat_membership = []
     for i in range(len(mat_cluster_centers)):
         vec_cluster_distances = []
@@ -50,6 +100,15 @@ def mahalanobis_cauchy_membership_matrix(mat_entries, mat_cluster_centers, ten_c
 
 
 def mahalanobis_own_old_distribution(mat_entries, mat_cluster_centers, ten_covariances):
+    """
+    Here each membership_value = own_distribution(1, 0.5, manhattan_distance(entry_vector, cluster_center_vector))
+
+    :param mat_entries: vector of entry vectors (matrix), len(entry_vector) = len(cluster_center_vector)
+    :param mat_cluster_centers: vector of cluster centers vectors (matrix)
+    :param ten_covariances: vector of covariance matrices
+    :return: membership matrix (vector of cluster vector membership values)
+    """
+
     mat_membership = []
     for i in range(len(mat_cluster_centers)):
         vec_cluster_distances = []
@@ -63,6 +122,13 @@ def mahalanobis_own_old_distribution(mat_entries, mat_cluster_centers, ten_covar
 
 
 def norm_membership_matrix_for_clusters(mat_membership):
+    """
+    Normalize values dependent for each cluster
+
+    :param mat_membership: vector of cluster vector membership values
+    :return: normalized membership matrix
+    """
+
     vec_summarized_parameters = []
     mat_norm_membership = []
     for j in range(len(mat_membership)):
@@ -81,6 +147,13 @@ def norm_membership_matrix_for_clusters(mat_membership):
 
 
 def norm_membership_matrix_for_entries(mat_membership):
+    """
+    Normalize values for each entry
+
+    :param mat_membership: vector of cluster vector membership values
+    :return: normalized membership matrix
+    """
+
     vec_summarized_parameters = []
     mat_norm_membership = []
     for j in range(len(mat_membership[0])):
