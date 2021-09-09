3a4428dae19715ab0e369e894894eb8a64c9cbca
VolDonets <vovan.s.marsa@gmail.com>
2021-09-09 13:51:47 +0300

    I have fixed a bug with an empty clusters.

--- cmeans_math/covariances.py
+++ cmeans_math/covariances.py
@@ -15,16 +15,23 @@ from scipy import linalg
 ARRAY_LAMBDA = 0.001
 
 
-def np_covariance_matrix(mat_entries, vec_cluster_indexes):
+def np_covariance_matrix(mat_entries, vec_cluster_indexes, vec_cluster_center):
     """
     :param mat_entries: vector of entry vectors
     :param vec_cluster_indexes: vector of indexes selected entries
+    :param vec_cluster_center: vector of cluster center coordinates
     :return: numpy covariance matrix
     """
 
     mat_cluster_entries = []
-    for inx in vec_cluster_indexes:
-        mat_cluster_entries.append(mat_entries[inx])
+    if len(vec_cluster_indexes) != 0:
+        for inx in vec_cluster_indexes:
+            mat_cluster_entries.append(mat_entries[inx])
+    else:
+        mat_cluster_entries.append(vec_cluster_center)
+        mat_cluster_entries.append(vec_cluster_center[:])
+    if len(vec_cluster_indexes) == 1:
+        mat_cluster_entries.append(vec_cluster_center)
     npmat_cluster_entries = np.array(mat_cluster_entries).T
     npmat_covariance = np.cov(npmat_cluster_entries)
     return npmat_covariance
@@ -41,17 +48,18 @@ def np_inverse_covariance(npmat_covariance):
     return linalg.inv(npmat_covariance)
 
 
-def cluster_covariances(mat_entries, mat_cluster_entry_indexes):
+def cluster_covariances(mat_entries, mat_cluster_entry_indexes, mat_cluster_centers):
     """
     :param mat_entries: mat_entries: vector of entry vectors
     :param mat_cluster_entry_indexes: vector of vectors with indexes
            of entries (in mat_entries) for entries of each cluster
+    :param mat_cluster_centers: vector of cluster centers vectors
     :return: prepared clustering inverse covariance matrix
     """
 
     ten_covariances = []
     for i in range(len(mat_cluster_entry_indexes)):
-        npmat_covariance = np_covariance_matrix(mat_entries, mat_cluster_entry_indexes[i])
+        npmat_covariance = np_covariance_matrix(mat_entries, mat_cluster_entry_indexes[i], mat_cluster_centers[i])
         npmat_covariance = np_inverse_covariance(npmat_covariance)
         ten_covariances.append(npmat_covariance.tolist())
     return ten_covariances
