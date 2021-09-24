e0952266bfb5d72f117e035995a0d8f6e2e1f2ee
VolDonets <vovan.s.marsa@gmail.com>
2021-09-24 21:23:54 +0300

    I have added universal function for all divergences

--- cmeans_math/divergences.py
+++ cmeans_math/divergences.py
@@ -28,3 +28,15 @@ def cross_entropy(var_old_distance, var_new_distance):
     if var_old_distance == 0:
         var_old_distance = SMALL_VALUE
     return var_new_distance * math.log2(var_old_distance)
+
+
+def divergence(mat_membership, mat_old_membership,
+               divergence_func=kulback_leibler_divergence):
+    mat_divergence = []
+    for j in range(len(mat_membership)):
+        vec_divergence = []
+        for i in range(len(mat_membership[0])):
+            vec_divergence.append(divergence_func(mat_old_membership[j][i],
+                                                  mat_membership[j][i]))
+        mat_divergence.append(vec_divergence)
+    return mat_divergence
