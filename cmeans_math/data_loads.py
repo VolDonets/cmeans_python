e927c1116473edf5f311ef07abbc8cfb639757f8
VolDonets <vovan.s.marsa@gmail.com>
2021-09-27 02:15:53 +0300

    Here is a data loads for urology dataset.

--- cmeans_math/data_loads.py
+++ cmeans_math/data_loads.py
@@ -31,3 +31,28 @@ def load_iris_data(path=""):
         vec_check.append(vec_check_only_names.index(x))
 
     return BasicDataSaver(vec_names, mat_data, vec_check)
+
+
+def load_urology_cleaned():
+    data = np.loadtxt("test_data/medical_data_cleaned/names.txt", dtype='str', delimiter='|')
+    vec_names = data.tolist()
+    vec_names.pop(0)
+
+    data = np.loadtxt("test_data/medical_data_cleaned/data.txt", dtype='float', delimiter='|')
+    mat_data = data.tolist()
+
+    for vec_data in mat_data:
+        vec_data.pop(0)
+
+    data = np.loadtxt("test_data/medical_data_cleaned/check.txt", dtype='str', delimiter='|')
+    vec_name_check = data.tolist()
+
+    vec_check = []
+    vec_check_only_names = []
+    for x in vec_name_check:
+        if x not in vec_check_only_names:
+            vec_check_only_names.append(x)
+    for x in vec_name_check:
+        vec_check.append(vec_check_only_names.index(x))
+
+    return BasicDataSaver(vec_names, mat_data, vec_check)
