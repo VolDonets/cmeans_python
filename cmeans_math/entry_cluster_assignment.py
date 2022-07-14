a544c9247582b55ce19dbc97b495ed6e83219e33
VolDonets <vovan.s.marsa@gmail.com>
2022-07-14 13:22:48 +0300

    Added divergence cluster assignment.

--- cmeans_math/entry_cluster_assignment.py
+++ cmeans_math/entry_cluster_assignment.py
@@ -132,3 +132,52 @@ def mahalanobis_own_old_distribution_cluster_assignment(mat_entries, mat_cluster
         var_selected_cluster = vec_distance_to_clusters.index(min(vec_distance_to_clusters))
         mat_cluster_entry_indexes[var_selected_cluster].append(entry_inx)
     return mat_cluster_entry_indexes
+
+
+def divergence_cluster_assignment(mat_entries, mat_cluster_centers, ten_covariances,
+                                  divergence_func=divergences.kulback_leibler_divergence,
+                                  basic_distance="Manhattan"):
+    mat_cluster_entry_indexes = []
+    for cl_inx in range(len(mat_cluster_centers)):
+        mat_cluster_entry_indexes.append([])
+
+    for entry_inx in range(len(mat_entries)):
+        vec_distance_to_clusters = []
+        for cl_inx in range(len(mat_cluster_centers)):
+            var_distance = 0.0
+            if basic_distance == "Manhattan":
+                var_distance = distributions.\
+                    cauchy_distribution(var_etta=1,
+                                        var_distance=distances.manhattan_distance(mat_entries[entry_inx],
+                                                                                  mat_cluster_centers[cl_inx]))
+            elif basic_distance == "Mahalanobis":
+                var_distance = distributions.\
+                    cauchy_distribution(var_etta=1,
+                                        var_distance=distances.mahalanobis_distance(mat_entries[entry_inx],
+                                                                                    mat_cluster_centers[cl_inx]))
+            vec_distance_to_clusters.append(var_distance)
+        var_selected_cluster = vec_distance_to_clusters.index(max(vec_distance_to_clusters))
+        # var_selected_cluster = vec_distance_to_clusters.index(min(vec_distance_to_clusters))
+        mat_cluster_entry_indexes[var_selected_cluster].append(entry_inx)
+
+    # a bit ducking cracking
+    # so if we have an empty cluster // less than 5 elements I'll separate too huge one
+    while True :
+        var_emp_cluster_inx = -1
+        var_biggest_cluster_inx = 0
+        for inx in range(len(mat_cluster_entry_indexes)):
+            if len(mat_cluster_entry_indexes[inx]) < 2:
+                var_emp_cluster_inx = inx
+                break
+        if var_emp_cluster_inx == -1:
+            break
+        for inx in range(len(mat_cluster_entry_indexes)):
+            if len(mat_cluster_entry_indexes[inx]) > len(mat_cluster_entry_indexes[var_biggest_cluster_inx]):
+                var_biggest_cluster_inx = inx
+
+        var_count_moved_indexes = 2
+        for i in range(var_count_moved_indexes):
+            mat_cluster_entry_indexes[var_emp_cluster_inx].append(mat_cluster_entry_indexes[var_biggest_cluster_inx][0])
+            del mat_cluster_entry_indexes[var_biggest_cluster_inx][0]
+
+    return mat_cluster_entry_indexes
